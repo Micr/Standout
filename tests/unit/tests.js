c0c0e5bbb019a7d7c42a98a1c39a9582a9e9dff4
Micr <asdfasdfadfasdfsad@gmail.com>
2013-09-21 21:13:19 +0400

    Initial commit

---
+++ tests/unit/tests.js
@@ -0,0 +1,7 @@
+module('main');
+test('Should show that jQuery is included', function () {
+	ok(window.jQuery, 'jQuery is included');
+});
+test('Should show that jQuery is included', function () {
+	ok(window.jQuery, 'jQuery is included');
+});
